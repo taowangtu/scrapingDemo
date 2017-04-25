7f8df7486809d609b7392a9c341773b4d35ed033
taowangtu <mengxb01@126.com>
2017-04-25 21:59:40 +0800

    change wikipedia_mysql.py file,mysql source

--- scrapingDemo/wikipedia_mysql.py
+++ scrapingDemo/wikipedia_mysql.py
@@ -5,7 +5,7 @@ import datetime
 import random
 import pymysql
 
-conn=pymysql.connect(host='127.0.0.1',user='root',passwd='111111',db='scraping',charset='utf8')
+conn=pymysql.connect(host='192.168.99.147',user='root',passwd='111111',db='scraping',charset='utf8')
 
 cur=conn.cursor()
 cur.execute("USE scraping;")
